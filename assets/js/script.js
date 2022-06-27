12f3462d863e504cc08a95762f9b119eaf3d08ff
Veronica Gonzalez <veronicagonzalez@serviciodigital.cl>
2022-06-27 19:25:53 -0400

    animacion del submenu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,47 @@
 // Animación Navbar
 window.addEventListener("DOMContentLoaded", (event) => {
+    // El navbar se achica cuando la página hace scroll
+    document.addEventListener("scroll", scroll("#navbar", "nav100"));
+    document.addEventListener("scroll", scroll("#navegacion", "nav4rem"));
+    document.addEventListener("scroll", scroll("#submenu", "sub6rem"));
+});
+
+/**
+ * Este metodo valida si el scrollY es 0 y en ese caso quita la clase recibida por parametro,
+ * en caso contrario la agrega
+ * 
+ * param selector al cual será aplicada la clase
+ * param clase a aplicar
+ * returns 
+ */
+function scroll(selector, clase) {
     var navHeight = function() {
-        const navbar = document.body.querySelector("#navbar");
+        // Se obtiene el elemento a agregar o quitar la clase, el nombre es recibido por parametro
+        const navegacion = document.body.querySelector(selector);
+
         if (window.scrollY === 0) {
-            navbar.classList.remove("nav100");
+            // Se elimina la clase recibida por parametro
+            navegacion.classList.remove(clase);
         } else {
-            navbar.classList.add("nav100");
+            // Se agrega la clase recibida por parametro
+            navegacion.classList.add(clase);
         }
     };
 
-    // El nav se achica cuando la página hace scroll
-    document.addEventListener("scroll", navHeight);
-});
-
-// Menu mobile animado - Corrección de top
-window.addEventListener("DOMContentLoaded", (event) => {
-    var navHeight = function() {
-        const navegacion = document.body.querySelector("#navegacion");
-        if (window.scrollY === 0) {
-            navegacion.classList.remove("nav4rem");
-        } else {
-            navegacion.classList.add("nav4rem");
-        }
-    };
-
-    // El nav se achica cuando la página hace scroll
-    document.addEventListener("scroll", navHeight);
-});
-
+    // Se retorna la funcion creada con los parametros recibidos
+    return navHeight;
+}
 
 // Animación Ícono Menú
 let elementoMenu = document.getElementsByClassName('icono-menu')[0];
 
 elementoMenu.addEventListener('click', function(event) {
     elementoMenu.classList.toggle('activo');
+});
+
+// Submenu desplegable
+let menuPadre = document.getElementsByClassName('enlace')[1];
+
+menuPadre.addEventListener('click', function(event) {
+    menuPadre.classList.toggle('activo');
 });
