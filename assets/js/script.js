2eb018389c855a1302bc326792861cbf367049a0
Veronica Gonzalez <veronicagonzalez@serviciodigital.cl>
2022-06-28 14:34:30 -0400

    Bloque acordión

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,3 +57,20 @@ let iconoSubmenu = document.getElementsByClassName('icono-submenu')[0];
 iconoSubmenu.addEventListener('click', function(event) {
     iconoSubmenu.classList.toggle('activo');
 });
+
+
+// Bloque acordión 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
