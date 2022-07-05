060c7048deec742cdad9f140d2591600eb6a1784
Veronica Gonzalez <veronicagonzalez@serviciodigital.cl>
2022-07-04 20:41:12 -0400

    WhatsApp flotante

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,6 +37,14 @@ let elementoMenu = document.getElementsByClassName('icono-menu')[0];
 
 elementoMenu.addEventListener('click', function(event) {
     elementoMenu.classList.toggle('activo');
+
+    // Secciones opacity, se obtienen todas las secciones como un listado HTMLCollection
+    let secciones = document.getElementsByClassName('flex');
+
+    // Se recorre uno por uno los elementos y se le agrega la clase opacity
+    for (var i = 0; i < secciones.length; i++) {
+        secciones[i].classList.toggle('clase-opacity');
+    }
 });
 
 // Submenu desplegable, se obtienen todos los enlaces como un listado HTMLCollection
