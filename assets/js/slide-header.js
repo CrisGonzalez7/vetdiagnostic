aef0766adb02b599382326a9b82ba9714b47949f
Veronica Gonzalez <veronicagonzalez@serviciodigital.cl>
2023-02-09 19:27:59 -0300

    Correccion Slide Header

--- assets/js/slide-header.js
+++ assets/js/slide-header.js
@@ -1,21 +1,21 @@
-
 var slideshows = document.querySelectorAll('[data-component="slideshow"]');
 slideshows.forEach(initSlideShow);
 
 function initSlideShow(slideshow) {
 
-	var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
+    var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
 
-	var index = 0, time = 5000;
-	slides[index].classList.add('active');
+    var index = 0,
+        time = 5000;
+    slides[index].classList.add('active');
 
-	setInterval( () => {
-		slides[index].classList.remove('active');
-		
-		index++;
-		if (index === slides.length) index = 0;
+    setInterval(() => {
+        slides[index].classList.remove('active');
 
-		slides[index].classList.add('active');
+        index++;
+        if (index === slides.length) index = 0;
 
-	}, time);
+        slides[index].classList.add('active');
+
+    }, time);
 }
