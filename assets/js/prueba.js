5588078d40f4d7241875f4a4b15a029d2b568eeb
Veronica Gonzalez <veronicagonzalez@serviciodigital.cl>
2023-02-15 17:38:30 -0300

    Trabajando en el Header

---
+++ assets/js/prueba.js
@@ -0,0 +1,11 @@
+// pruebas
+
+$(".icono-menu").on("click", function() {
+    $(this).toggleClass("activo");
+});
+
+
+$('#toggle').addClass('active');
+
+let toggle = document.querySelector('#toggle');
+toggle.classList.add('active');
